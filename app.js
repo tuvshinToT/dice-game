eba98dd79cb7c8b5d7a7102456d96b5f7b7fdc85
ToTe <tuvshintor120@gmail.com>
2025-11-21 16:36:27 +0800

    uu saihan active playeruudiine shn soligddog bologloo

--- app.js
+++ app.js
@@ -23,11 +23,39 @@ document.getElementById('current-1').textContent = "0";
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
 
+
+//shoog shideh event listener
 document.querySelector(".btn-roll").addEventListener("click",function shooShid(){
+    // 1 - 6 dotorh sanamsargui neg too gargaj awna
     var diceNumber = Math.floor(Math.random() * 6) + 1;
 
+    //shoonii zurgiig web deer gargaj irne
     diceDom.style.display = "block";
+
+    //buusan sanamsargui toond hargalzah shoonii zurgiig web deer gargaj ireh
     diceDom.src = 'dice-' + diceNumber + '.png';
+
+    //buusan too ni 1 - ees ylgaatai bol idewhitei toglogchiin eeljiin onoog nemegduulne
+    if(diceNumber !== 1){
+        //1 - ees ylgaatai too buulaa
+        roundScore += diceNumber;
+        document.getElementById('current-' + activePlayer).textContent = roundScore;
+    } else {
+        //1 buusan tul toglogchiin eeljiig ene hesegt solino
+
+        //ene toglogchiin eeljiindee tsugluulsan onooog 0 bolgono
+        roundScore = 0;
+        document.getElementById('current-' + activePlayer).textContent = 0;
+
+        //toglogchiin eeljiig nuguu togloogch ruu shiljuulne
+        activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        //shoog tur alga bolgono
+        diceDom.style.display = "none";
+    }    
 
 
 });
